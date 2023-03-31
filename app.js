c5c2a5a5d6c89e59d2421c5a255b8ba8cd2d8ab3
Mohit Hooda <mohit@Mohits-MacBook-Air.local>
2023-03-31 16:59:55 +0530

    Completed proj -1

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      firstName: '',
+      lastName: ''
     };
   },
+  computed: {
+    fullName(){
+      if(this.firstName === '' || this.lastName === ''){
+        return ''
+      }else{
+        return this.firstName + ' ' + this.lastName
+      }
+    }
+  },
+  methods: {
+    add(num){
+      this.counter = this.counter + num;
+    },
+    remove(num){
+      this.counter = this.counter - num;
+    },
+    resetEntries(){
+      this.counter = 0,
+      this.firstName= ''
+      this.lastName= ''
+    },
+   
+  } 
 });
 
 app.mount('#events');
